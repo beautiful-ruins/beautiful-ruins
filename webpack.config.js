fd87a11096ea1b3acfa6f1d4cca5f485879c7b6c
Purab Shah <purabshah8@gmail.com>
2019-02-21 14:01:57 -0500

    update config

--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,22 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    query: {
+                        presets: ['@babel/env', '@babel/react']
+                    }
+                },
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '*']
+    },
     devtool: 'source-map',
 };
